22a5db46a985aed8d453dd692b6d192af9c705d5
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-10-09 23:31:52 -0300

    adding profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig),    
   ],
   providers: [],
   bootstrap: [AppComponent]
