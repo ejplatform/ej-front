34fcfce37acc9a653cb99391005449d798c64985
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-10-24 18:45:43 -0300

    return csrftoken as factory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,7 @@ export function HttpLoaderFactory(http: HttpClient) {
 }
 
 export function xsrfFactory() {
-  new CookieXSRFStrategy('RESPONSE_TOKEN', 'RESPONSE_TOKEN');
+  return new CookieXSRFStrategy('csrftoken', 'X-CSRFToken');
 }
 
 @NgModule({
