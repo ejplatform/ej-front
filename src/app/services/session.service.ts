345748598c11356dbd89cf3269b1cece3f896514
Bruno Martin <bruno.martin@hacklab.com.br>
2017-10-30 16:49:05 -0200

    beaultify: fix identations and other formating stuff, please config editor to use .editorconfig patterns

--- src/app/services/session.service.ts
+++ src/app/services/session.service.ts
@@ -5,29 +5,28 @@ import { Profile } from '../models/profile';
 @Injectable()
 export class SessionService {
 
-    constructor(private localStorageService: LocalStorageService) { }
+  constructor(private localStorageService: LocalStorageService) { }
 
-    destroy() {
-        this.localStorageService.clear('currentProfile');
-        this.localStorageService.clear('token');        
-    };
+  destroy() {
+      this.localStorageService.clear('currentProfile');
+      this.localStorageService.clear('token');
+  };
 
-    setProfile(profile: Profile): Profile {
-        this.localStorageService.store('currentProfile', profile);
-        return this.localStorageService.retrieve('currentProfile');
-    };
+  setProfile(profile: Profile): Profile {
+      this.localStorageService.store('currentProfile', profile);
+      return this.localStorageService.retrieve('currentProfile');
+  };
 
-    currentProfile(): Profile {
-        return this.localStorageService.retrieve('currentProfile');
-    };
+  currentProfile(): Profile {
+      return this.localStorageService.retrieve('currentProfile');
+  };
 
-    setToken(token: string): string {
-        this.localStorageService.store('token', token);
-        return this.localStorageService.retrieve('token');
-    };
+  setToken(token: string): string {
+      this.localStorageService.store('token', token);
+      return this.localStorageService.retrieve('token');
+  };
 
-    getToken(): string {
-        return this.localStorageService.retrieve('token');
-    };
-
+  getToken(): string {
+      return this.localStorageService.retrieve('token');
+  };
 }
--- src/app/services/social-facebook.service.ts
+++ src/app/services/social-facebook.service.ts
@@ -5,38 +5,33 @@ import { LoginResponse } from 'ngx-facebook';
 
 import { AuthService } from './auth.service';
 
-
 @Injectable()
 export class SocialFacebookService {
 
   constructor(private authService: AuthService, private fb: FacebookService) {
-    
-        let initParams: InitParams = {
-          appId: '1757744567588794',
-          xfbml: true,
-          version: 'v2.10'
-        };
-    
-        fb.init(initParams);
-    
-    }
 
-    login(){
-        this.fb.getLoginStatus().then((response:LoginResponse) =>{
-            if(response.status != 'connected'){
-                this.fb.login().then((response: LoginResponse) => {
-                    this.authService.signInFacebook(response.authResponse.accessToken).subscribe();
-                }).catch((error: any) =>{
-                    console.error(error);
-                });
+    let initParams: InitParams = {
+      appId: '1757744567588794',
+      xfbml: true,
+      version: 'v2.10'
+    };
 
-            }else{
-                this.authService.signInFacebook(response.authResponse.accessToken).subscribe();                
-            }
-        }).catch((error: any) => {
-            console.log(error);
-        });
+    fb.init(initParams);
+  }
 
-    }
-
+  login(){
+    this.fb.getLoginStatus().then((response:LoginResponse) =>{
+        if(response.status != 'connected'){
+            this.fb.login().then((response: LoginResponse) => {
+                this.authService.signInFacebook(response.authResponse.accessToken).subscribe();
+            }).catch((error: any) =>{
+                console.error(error);
+            });
+        }else{
+            this.authService.signInFacebook(response.authResponse.accessToken).subscribe();
+        }
+    }).catch((error: any) => {
+        console.log(error);
+    });
+  }
 }
