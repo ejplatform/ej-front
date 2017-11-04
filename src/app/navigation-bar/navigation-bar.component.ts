8aaaad5f895d374c9370dc4c8277bdbd7e8d60ea
Leandro Nunes dos Santos <leandronunes@gmail.com>
2017-11-03 22:16:43 -0300

    automatic close navbar after click on small devices closes #66

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+
 import * as _ from 'lodash' 
 
 import { Profile } from '../models/profile';
 import { ProfileService } from '../services/profile.service';
+import { GlobalState } from '../global.state';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -12,14 +14,28 @@ import { ProfileService } from '../services/profile.service';
 export class NavigationBarComponent implements OnInit {
 
   @Input() profile: Profile;
+  isMenuCollapsed: boolean = false;
+  static MAX_SIZE_FOR_AUTOMATIC_TOGGLE = 640
   
-  constructor(private profileService: ProfileService) {
+  constructor(private _state: GlobalState,  private profileService: ProfileService) {
+    this._state.subscribe('menu.isCollapsed', (isCollapsed) => {
+      this.isMenuCollapsed = isCollapsed;
+    });
   }
-
+ 
   ngOnInit() {
     if(_.isUndefined(this.profile.image)){
       this.profile.image = '/assets/images/icons/profile_icon.svg';
     }
+  }
+
+  toggleMenu() {
+    if(window.innerWidth > NavigationBarComponent.MAX_SIZE_FOR_AUTOMATIC_TOGGLE)
+      return false 
+
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+    this._state.notifyDataChanged('menu.isCollapsed', this.isMenuCollapsed);
+    return false;
   }
 
 }
