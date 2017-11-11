34551c6479483bc1527bbfb698ab1f130418f6e6
Bruno Martin <bruno.martin@hacklab.com.br>
2017-11-11 15:21:51 -0200

    beautify, add scope to facebook login options to get email from graph api

--- src/app/services/social-facebook.service.ts
+++ src/app/services/social-facebook.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, Inject, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FacebookService, InitParams } from 'ngx-facebook';
-import { LoginResponse } from 'ngx-facebook';
+import { FacebookService, InitParams, LoginResponse, LoginOptions } from 'ngx-facebook';
 
 import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
@@ -23,24 +22,20 @@ export class SocialFacebookService {
   }
 
   login(){
-    this.fb.getLoginStatus().then((response:LoginResponse) =>{
-      if(response.status != 'connected'){
-          this.fb.login().then((response: LoginResponse) => {
-              this.authService.signInFacebook(response.authResponse.accessToken).subscribe(
-                (resp) => this.loginReturn.emit(resp),
-                (error) => this.loginReturn.emit(error)
-              );
-          }).catch((error: any) =>{
-              console.error(error);
-          });
-      }else {
-          this.authService.signInFacebook(response.authResponse.accessToken).subscribe(
-            (resp) => this.loginReturn.emit(resp),
-            (error) => this.loginReturn.emit(error)
-          );
-      }
-    }).catch((error: any) => {
-      console.log(error);
+
+    const loginOptions: LoginOptions = {
+      enable_profile_selector: true,
+      return_scopes: true,
+      scope: 'public_profile, user_friends, email'
+    };
+
+    this.fb.login(loginOptions).then((response: LoginResponse) => {
+        this.authService.signInFacebook(response.authResponse.accessToken).subscribe(
+          (resp) => this.loginReturn.emit(resp),
+          (error) => this.loginReturn.emit(error)
+        );
+    }).catch((error: any) =>{
+        console.error(error);
     });
   }
 }
