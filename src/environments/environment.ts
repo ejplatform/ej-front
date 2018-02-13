190b6bd3325a783972588d60ba876d9216183adb
Laury Bueno <laury@hacklab.com.br>
2018-02-13 11:47:36 -0200

    Sentry added as a error logging utility

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,5 +8,6 @@ export const environment = {
   production: false,
   apiUrl: '',
   polisUrl: 'https://brasilqueopovoquer.hacklab.com.br/',
-  onSignalAppId: '660977f0-30ad-4f69-a9a4-2873b8f5de7e'
+  onSignalAppId: '660977f0-30ad-4f69-a9a4-2873b8f5de7e',
+  sentryDSN: ''
 };
