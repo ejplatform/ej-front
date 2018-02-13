d6828d06aad63f878a2d4b11da295c999f94f525
Laury Bueno <laury@hacklab.com.br>
2018-02-13 15:20:23 -0200

    [Env] apiUrl doesn't need to be set explicitly

--- src/environments/environment.homolog.ts
+++ src/environments/environment.homolog.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dev.app.brasilqueopovoquer.org.br',
+  apiUrl: '',
   facebookAppId: '1757744567588794',
   polisUrl: 'https://brasilqueopovoquer.hacklab.com.br/',
   onSignalAppId: '660977f0-30ad-4f69-a9a4-2873b8f5de7e',
