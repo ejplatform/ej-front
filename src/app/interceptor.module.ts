6791152a7e7a4fd7eda19d830f6fb32ca9ee4cf6
Leandro Nunes dos Santos <leandronunes@gmail.com>
2018-02-28 21:26:52 -0300

    force logout on 401 and 403 http status code response

--- src/app/interceptor.module.ts
+++ src/app/interceptor.module.ts
@@ -1,32 +1,36 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpClient } from '@angular/common/http';
 import { SessionService } from './services/session.service';
 
 import { CookieService } from 'ngx-cookie-service';
 
 import * as Raven from 'raven-js';
+import { AuthService } from './services/auth.service';
+import { environment } from '../environments/environment';
+import { Profile } from './models/profile';
+import { ProfileService } from './services/profile.service';
 
 @Injectable()
 export class HttpsRequestInterceptor implements HttpInterceptor {
 
-  constructor(private session: SessionService, private cookieService: CookieService) {}
+  constructor(private sessionService: SessionService, private inj: Injector, private cookieService: CookieService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.session.getToken();
+    const token = this.sessionService.getToken();
 
     // If there is an API key stored, send it in this request
     if (token) {
       // API token should never be sent to the polis backend
       if (!request.url.includes('polis.brasilqueopovoquer.org.br')) {
-        request = request.clone({headers: request.headers.set('Authorization', 'Token ' + token)});
+        request = request.clone({ headers: request.headers.set('Authorization', 'Token ' + token) });
       }
 
     } else {
 
       // If there is no key, allow cookies to be sent, but only if this is a request for an API key
       if (request.url.includes('/profile/key')) {
-        request = request.clone({withCredentials: true});
+        request = request.clone({ withCredentials: true });
       }
 
     }
@@ -35,7 +39,7 @@ export class HttpsRequestInterceptor implements HttpInterceptor {
     // However, don't do it when sending requests to Polis
     const csrftoken = this.cookieService.get('csrftoken');
     if (csrftoken && !request.url.includes('polis.brasilqueopovoquer.org.br')) {
-      request = request.clone({headers: request.headers.set('X-CSRFToken', csrftoken)});
+      request = request.clone({ headers: request.headers.set('X-CSRFToken', csrftoken) });
     }
 
     return next.handle(request).catch(error => {
@@ -48,7 +52,12 @@ export class HttpsRequestInterceptor implements HttpInterceptor {
       }
 
       if (error.status === 401 || error.status === 403) {
-        // TODO: the logout process must be triggered now
+        const auth = this.inj.get(AuthService);
+        const profileService = this.inj.get(ProfileService);
+
+        auth.signOut().subscribe( () => {
+          profileService.setProfile(null);
+        });
       }
 
       return Observable.throw(error);
