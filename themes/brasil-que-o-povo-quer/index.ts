74f4ef5983cd2947bf2009ee605424a2c778e8d1
Leandro Nunes dos Santos <leandronunes@gmail.com>
2018-04-07 18:45:39 -0300

    renaminh black theme to brasil que o povo quer theme

---
+++ themes/brasil-que-o-povo-quer/index.ts
@@ -0,0 +1,5 @@
+import { BrasilHeaderComponent } from './brasil-header/brasil-header.component';
+
+export let name = 'brasil-que-o-povo-quer';
+export let components: any = [];
+export let hotspots: any = [BrasilHeaderComponent];
