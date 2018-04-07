09babc2ababeca653e70e759db55706c1d087105
Leandro Nunes dos Santos <leandronunes@gmail.com>
2018-04-07 20:52:14 -0300

    put unit test to works

--- themes/brasil-que-o-povo-quer/header/header.component.spec.ts
+++ themes/brasil-que-o-povo-quer/header/header.component.spec.ts
@@ -1,49 +1,48 @@
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { TranslateModule } from '@ngx-translate/core';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { By } from '@angular/platform-browser';
-// import { NgPipesModule } from 'ngx-pipes';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { AuthService } from '../services/auth.service';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { NgPipesModule } from 'ngx-pipes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../../../src/app/services/auth.service';
 
 
-// import { HeaderComponent } from './header.component';
-// import * as helpers from '../../spec/helpers';
-// import { ProfileService } from '../services/profile.service';
-// import { GlobalState } from '../global.state';
-// import { CategoryService } from '../services/category.service';
+import { HeaderComponent } from './header.component';
+import * as helpers from '../../../src/spec/helpers';
+import { ProfileService } from '../../../src/app/services/profile.service';
+import { GlobalState } from '../../../src/app/global.state';
+import { CategoryService } from '../../../src/app/services/category.service';
+
+describe('HeaderComponent', () => {
+
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+  const mocks = helpers.getMocks();
 
 
-// describe('HeaderComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule, TranslateModule.forRoot(), NgbModule.forRoot(), NgPipesModule],
+      providers: [{ provide: CategoryService, useValue: mocks.categoryService },
+      { provide: GlobalState, useValue: mocks.globalState },
+      { provide: AuthService, useValue: mocks.authService },
+      { provide: ProfileService, useValue: mocks.profileService },
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    })
+      .compileComponents();
+  }));
 
-//   let component: HeaderComponent;
-//   let fixture: ComponentFixture<HeaderComponent>;
-//   const mocks = helpers.getMocks();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
+  it('display site-header', () => {
+    expect(fixture.debugElement.queryAll(By.css('.site-header')).length).toBe(1);
+  });
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [HeaderComponent],
-//       imports: [RouterTestingModule, TranslateModule.forRoot(), NgbModule.forRoot(), NgPipesModule],
-//       providers: [{ provide: CategoryService, useValue: mocks.categoryService },
-//       { provide: GlobalState, useValue: mocks.globalState },
-//       { provide: AuthService, useValue: mocks.authService },
-//       { provide: ProfileService, useValue: mocks.profileService },
-//       ],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-//     })
-//       .compileComponents();
-//   }));
-
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(HeaderComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-
-//   it('display site-header', () => {
-//     expect(fixture.debugElement.queryAll(By.css('.site-header')).length).toBe(1);
-//   });
-
-// });
+});
