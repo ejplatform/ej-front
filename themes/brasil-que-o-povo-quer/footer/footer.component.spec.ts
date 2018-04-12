12a203ee4cd85c9d42660395d4760f4fef77794b
Lucas Malta <lucas50xd@hotmail.com>
2018-04-11 21:37:25 -0300

    setup theme-footer

---
+++ themes/brasil-que-o-povo-quer/footer/footer.component.spec.ts
@@ -0,0 +1,38 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+
+
+import { FooterComponent } from './footer.component';
+import * as helpers from '../../spec/helpers';
+import { GlobalState } from '../global.state';
+import { CategoryService } from '../services/category.service';
+
+describe('FooterComponent', () => {
+
+  let component: FooterComponent;
+  let fixture: ComponentFixture<FooterComponent>;
+  const mocks = helpers.getMocks();
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [FooterComponent],
+      providers: [{ provide: CategoryService, useValue: mocks.categoryService }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FooterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('display component', () => {
+    expect(fixture.debugElement.queryAll(By.css('.sponsors')).length).toBe(1);
+  });
+
+});
