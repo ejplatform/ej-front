15eb22082673fcb771336fc37b859d519b3d85be
Lucas Malta <lucas50xd@hotmail.com>
2018-04-12 00:29:08 -0300

    makes brasil-que-o-povo-quer footer services work again

--- themes/brasil-que-o-povo-quer/footer/footer.component.ts
+++ themes/brasil-que-o-povo-quer/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
-// import { Category } from '../models/category';
-// import { CategoryService } from '../services/category.service';
+import { Category } from '../../../src/app/models/category';
+import { CategoryService } from '../../../src/app/services/category.service';
 import { Hotspot } from '../../../src/app/hotspot/hotspot.decorator';
 
 
@@ -14,10 +14,10 @@ export class FooterComponent {
 
   styles: any = null;
 
-  constructor(/*private categoryService: CategoryService*/) {
-    // categoryService.categoryChangeEvent.subscribe( (category: Category) => {
-      // this.styles = category ? category.getStyle() : null;
-    // });
+  constructor(private categoryService: CategoryService) {
+    categoryService.categoryChangeEvent.subscribe( (category: Category) => {
+      this.styles = category ? category.getStyle() : null;
+    });
   }
 
 }
