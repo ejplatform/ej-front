846191d7733c462885a1e7c80acc27c20f3d7c26
Lucas Malta <lucas50xd@hotmail.com>
2018-04-11 22:18:11 -0300

    adding theme-footer to hotspot list

--- themes/theme-default/footer/footer.component.ts
+++ themes/theme-default/footer/footer.component.ts
@@ -1,20 +1,11 @@
 import { Component, Input } from '@angular/core';
-// import { Category } from '../models/category';
-// import { CategoryService } from '../services/category.service';
+import { Hotspot } from '../../../src/app/hotspot/hotspot.decorator';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'theme-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
+@Hotspot('theme_footer')
 export class FooterComponent {
-
-  styles: any = null;
-
-  constructor(/*private categoryService: CategoryService*/) {
-    // categoryService.categoryChangeEvent.subscribe( (category: Category) => {
-      // this.styles = category ? category.getStyle() : null;
-    // });
-  }
-
 }
