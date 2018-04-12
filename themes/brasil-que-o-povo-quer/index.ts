cd06e5797bb0d62f7159d8c94942488b0eacb028
Lucas Malta <lucas50xd@hotmail.com>
2018-04-12 15:33:26 -0300

    making footer available to brasil-que-o-povo-quer

--- themes/brasil-que-o-povo-quer/index.ts
+++ themes/brasil-que-o-povo-quer/index.ts
@@ -1,6 +1,7 @@
 import { HeaderComponent } from './header/header.component';
 import { ThemeMenuComponent } from './theme-menu/theme-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 export let name = 'brasil-que-o-povo-quer';
 export let components: any = [];
-export let hotspots: any = [ HeaderComponent, ThemeMenuComponent ];
+export let hotspots: any = [ HeaderComponent, ThemeMenuComponent, FooterComponent ];
