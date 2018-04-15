7bc17d0b19328254244731c11e8562d45c1720f5
Lucas Malta <lucas50xd@hotmail.com>
2018-04-15 18:40:43 -0300

    configures come-add component in default theme

--- themes/theme-default/index.ts
+++ themes/theme-default/index.ts
@@ -1,7 +1,13 @@
 import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComeAddComponent } from './come-add/come-add.component';
 
 export let name = 'default';
 export let components: any = [];
-export let hotspots: any = [ HeaderComponent, TopHeaderComponent, FooterComponent ];
+export let hotspots: any = [ 
+                HeaderComponent,
+                TopHeaderComponent, 
+                FooterComponent, 
+                ComeAddComponent 
+                ];
